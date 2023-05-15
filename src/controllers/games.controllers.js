57db8c092e034885a8e915695561946021621c3a
mickalala <mickalarissa01@gmail.com>
2023-05-15 12:09:28 -0300

    inserindo jogos

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,17 +1,30 @@
 import { db } from "../database/database.connection.js";
+import joi from "joi"
 
 export async function getGames(req, res) {
     try {
         const allGames = await db.query(`SELECT * FROM games;`)
         console.table(allGames.rows)
-        res.status(200).send(allGames.rows[0])
+        res.status(200).send(allGames.rows)
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
 export async function insertGames(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body
+    const gameSchema = joi.object({
+        name: joi.string().required(),
+        image: joi.string().required(),
+        stockTotal: joi.number().required(),
+        pricePerDay: joi.number().required()
+    })
+    const validation = gameSchema.validate(req.body, { abortEarly: false })
+
     try {
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
+        VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay])
+        res.sendStatus(201)
 
     } catch (err) {
         res.status(500).send(err.message)
